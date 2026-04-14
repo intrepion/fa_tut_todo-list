312c4278397a621b54275b6f7f4865d689f73bff
Oliver Forral <intrepion@gmail.com>
2026-04-13 23:48:14 -0600

    2. Green: Load The Current Task List

--- workspace/lib/code/task_list_controller.dart
+++ workspace/lib/code/task_list_controller.dart
@@ -1,1 +1,23 @@
+import '../contracts/task_api.dart';
+import '../contracts/task_list_response.dart';
 
+class TaskListViewModel {
+  final List<String> tasks;
+  final List<String> lines;
+  final String? errorMessage;
+
+  const TaskListViewModel({
+    required this.tasks,
+    required this.lines,
+    this.errorMessage,
+  });
+
+  factory TaskListViewModel.fromResponse(TaskListResponse response) {
+    return TaskListViewModel(tasks: response.tasks, lines: response.lines);
+  }
+}
+
+Future<TaskListViewModel> loadTasks(TaskApi api) async {
+  final response = await api.getTasks();
+  return TaskListViewModel.fromResponse(response);
+}
