44a3f512065949fb1f72830830465c0a4656e411
Oliver Forral <intrepion@gmail.com>
2026-04-13 19:06:48 -0600

    4. Green: Append And Remove Tasks

--- workspace/internal/code/task_list_service.go
+++ workspace/internal/code/task_list_service.go
@@ -7,3 +7,18 @@ func parseTaskStorage(storageText string) []string {
 	_ = json.Unmarshal([]byte(storageText), &tasks)
 	return tasks
 }
+
+func appendTask(taskList []string, taskText string) []string {
+	next := append([]string{}, taskList...)
+	return append(next, taskText)
+}
+
+func removeTaskByExactText(taskList []string, completedTaskText string) []string {
+	next := append([]string{}, taskList...)
+	for index, task := range next {
+		if task == completedTaskText {
+			return append(next[:index], next[index+1:]...)
+		}
+	}
+	return next
+}
