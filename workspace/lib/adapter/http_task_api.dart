93c031cb38702aea30dd39b647a334294ac80822
Oliver Forral <intrepion@gmail.com>
2026-04-13 23:51:09 -0600

    2. Green: Call The Canonical Task API Endpoints

--- workspace/lib/adapter/http_task_api.dart
+++ workspace/lib/adapter/http_task_api.dart
@@ -1,1 +1,45 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
+import '../contracts/task_api.dart';
+import '../contracts/task_list_response.dart';
+
+class HttpTaskApi implements TaskApi {
+  final String baseUrl;
+  final http.Client client;
+
+  HttpTaskApi({required this.baseUrl, http.Client? client})
+    : client = client ?? http.Client();
+
+  @override
+  Future<TaskListResponse> getTasks() async {
+    final response = await client.get(Uri.parse('$baseUrl/api/tasks'));
+    return TaskListResponse.fromJson(
+      jsonDecode(response.body) as Map<String, dynamic>,
+    );
+  }
+
+  @override
+  Future<TaskListResponse> addTask(String task) async {
+    final response = await client.post(
+      Uri.parse('$baseUrl/api/tasks'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'task': task}),
+    );
+    return TaskListResponse.fromJson(
+      jsonDecode(response.body) as Map<String, dynamic>,
+    );
+  }
+
+  @override
+  Future<TaskListResponse> removeTask(String task) async {
+    final uri = Uri.parse(
+      '$baseUrl/api/tasks',
+    ).replace(queryParameters: {'task': task});
+    final response = await client.delete(uri);
+    return TaskListResponse.fromJson(
+      jsonDecode(response.body) as Map<String, dynamic>,
+    );
+  }
+}
