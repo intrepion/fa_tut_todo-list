96bd18bcf49b60cad34759addbe971294853981c
Oliver Forral <intrepion@gmail.com>
2026-04-13 23:46:18 -0600

    Define todo-list Flutter contracts

--- workspace/lib/contracts/task_api.dart
+++ workspace/lib/contracts/task_api.dart
@@ -1,1 +1,7 @@
+import 'task_list_response.dart';
 
+abstract class TaskApi {
+  Future<TaskListResponse> getTasks();
+  Future<TaskListResponse> addTask(String task);
+  Future<TaskListResponse> removeTask(String task);
+}
--- workspace/lib/contracts/task_list_response.dart
+++ workspace/lib/contracts/task_list_response.dart
@@ -1,1 +1,13 @@
+class TaskListResponse {
+  final List<String> tasks;
+  final List<String> lines;
 
+  const TaskListResponse({required this.tasks, required this.lines});
+
+  factory TaskListResponse.fromJson(Map<String, dynamic> json) {
+    return TaskListResponse(
+      tasks: List<String>.from(json['tasks'] as List<dynamic>),
+      lines: List<String>.from(json['lines'] as List<dynamic>),
+    );
+  }
+}
